ea160d22c960a9aaae59fede9a088db723b0711f
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-24 15:27:18 -0400

    Add files via upload

---
+++ CSharp_CallStarWarsAPIAssignment/BusinessLogic/CharacterBL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using CSharp_CallStarWarsAPIAssignment.Models;
+
+namespace CSharp_CallStarWarsAPIAssignment.BusinessLogic
+{
+    public class CharacterBL
+    {
+        public async Task<List<StarWarsCharacterModel>> GetListOfAllCharacterInfo()
+        {            
+            string url = $"http://localhost:8086/api/StarWarsCharacters/";
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    };
+                    var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                    List<StarWarsCharacterModel> starWarsCharacters = JsonConvert.DeserializeObject<List<StarWarsCharacterModel>>(jsonString, settings);                  
+
+                    return starWarsCharacters;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<StarWarsCharacterModel> GetCharacterInfoByName(string name)
+        {
+            string url = $"http://localhost:8086/Api/StarWarsCharacters/GetCharacterByName?name={name}";
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    };
+                    var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                    StarWarsCharacterModel starWarsCharacter = JsonConvert.DeserializeObject<StarWarsCharacterModel>(jsonString, settings);
+
+                    return starWarsCharacter;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<List<StarWarsCharacterModel>> GetListOfCharacterInfoByAllegiance(string allegiance)
+        {
+            string url = $"http://localhost:8086/Api/StarWarsCharacters/GetCharactersByAllegiance/?allegiance={allegiance}";
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    };
+                    var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                    List<StarWarsCharacterModel> starWarsCharacters = JsonConvert.DeserializeObject<List<StarWarsCharacterModel>>(jsonString, settings);
+
+                    return starWarsCharacters;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<List<StarWarsCharacterModel>> GetListOfCharacterInfoByTrilogy(string trilogy)
+        {
+            string url = $"http://localhost:8086/Api/StarWarsCharacters/GetCharactersByTrilogy/?trilogy={trilogy}";
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    };
+                    var jsonString = response.Content.ReadAsStringAsync().Result;
+
+                    List<StarWarsCharacterModel> starWarsCharacters = JsonConvert.DeserializeObject<List<StarWarsCharacterModel>>(jsonString, settings);
+
+                    return starWarsCharacters;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp_CallStarWarsAPIAssignment/Models/StarWarsCharacterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_CallStarWarsAPIAssignment.Models
+{
+    public class StarWarsCharacterModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Allegiance { get; set; }
+        public bool IsJedi { get; set; }
+        public string IntroducedInTrilogy { get; set; }
+    }
+
+}
